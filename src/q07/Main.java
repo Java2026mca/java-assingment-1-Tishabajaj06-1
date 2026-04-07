0f3a095c39d76a43012abf4f9aa210601121b589
Tishabajaj06 <bajajtisha74@gmail.com>
2026-04-07 17:29:26 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -9,7 +9,6 @@ public class Main {
         }
         int swaps = 0;
         for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // swap
@@ -17,10 +16,8 @@ public class Main {
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     swaps++;
-                    swapped = true;
                 }
             }
-            if (!swapped) break;
         }
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i]);
