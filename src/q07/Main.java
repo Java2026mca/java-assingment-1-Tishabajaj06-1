c3e96a4bf6b7a4eaa1ab6c7f18d16efc8c070c17
Tishabajaj06 <bajajtisha74@gmail.com>
2026-04-07 17:38:13 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -4,14 +4,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         int swaps = 0;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
